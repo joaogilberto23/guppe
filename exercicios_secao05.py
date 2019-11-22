fd08431e55c8a33dcd97491157117b1d75332605
João Gilberto <joaogilberto_ns@yahoo.com.br>
2019-11-22 20:33:01 -0300

    Mais exercícios.

--- exercicios_secao05.py
+++ exercicios_secao05.py
@@ -10,3 +10,29 @@ elif num1 == num2:
     print('Os números são iguais!')
 else:
     print(f'O número maior é o: {num2}')
+
+"""
+Leia um número fornecido pelo usuário. Se esse número for positivo, caucule a raiz  quadrada
+do número. Se o número for negativo, mostre uma mensagem dizendo que o número é inválido.
+"""
+num = input('Digite um número: ')
+
+if float(num) < 0:
+    print('Número inválido!')
+else:
+    print(f'A raiz quadrada do número digitado é: {float(num) ** 0.5}')
+
+"""
+Leia um número real. Se o número  for positivo imprima a raiz quadrada. Do Contrário,
+imprima o número ao quadrado.
+"""
+num = input('Digite um número: ')
+
+if float(num) > 0:
+    print(f'A raiz quadrada do número digitado é: {float(num) ** 0.5}')
+else:
+    print(f'Este número ao quadrado é igual a: {float(num) ** 2}')
+
+"""
+
+"""
--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print(f'O número maior é o: {num1}')
+elif num1 == num2:
+    print('Os números são iguais!')
+else:
+    print(f'O número maior é o: {num2}')
