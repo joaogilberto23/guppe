5b39580c243aab5624849ff3aaca0c92b4b4805e
João Gilberto <joaogilberto_ns@yahoo.com.br>
2020-01-23 17:06:32 -0300

    Iniciando Loop / For

--- loop_for.py
+++ loop_for.py
@@ -10,7 +10,7 @@ for(int i=0; i<n; i++){
 
 Python:
 
-for item in interavel:
+for item in iterável:
     //execução do loop
 
 Loops são utilizados para iterar sobre sequência ou valores iteráveis
@@ -23,9 +23,39 @@ Exemplos:
     lista = [1, 2, 3, 4, 5]
 - Range
     range(1, 10)
+"""
+from builtins import print, range
 
+nome = 'João Gilberto'
+lista = [1, 3, 5, 7, 9]
+numeros = range(1, 10)  # Tem que transformar em uma lista
+
+# Exemplo de for 1 (Iterando em uma string)
+for letra in nome:
+    print(letra)
+
+# Exemplo de for 2 (Iterando em uma lista)
+for numero in lista:
+    print(numero)
+
+# Exemplo de for 3 (Iterando em um range)
+for numero in range(1, 10):
+    print(numero)
+"""
 OBS range: No range, o valor final é apenas uma referência, ele não é incluído na "impressão".
 """
+qtd = int(input('Quantas vezes esse loop deve rodar?'))
+for n in range(1, qtd + 1):
+    print(f'Imprimindo {n}')
 
+#############################################
 
+qtd = int(input('Quantas vezes esse loop deve rodar? '))
+soma = 0
 
+for n in range(1, qtd + 1):
+    num = int(input(f'Informe o {n}/{qtd} valor: '))
+    soma = soma + num
+print(f'A soma é {soma}')
+
+#############################################
---
+++ teste.py
@@ -0,0 +1,7 @@
+qtd = int(input('Quantas vezes esse loop deve rodar? '))
+soma = 0
+
+for n in range(1, qtd + 1):
+    num = int(input(f'Informe o {n}/{qtd} valor: '))
+    soma = soma + num
+print(f'A soma é {soma}')
